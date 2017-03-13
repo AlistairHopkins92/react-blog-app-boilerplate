6ca671aa9d3fec19bc318cd56c165cdf5175fbdd
AlistairHopkins92 <alistairhopkins92@gmail.com>
2017-03-13 11:45:37 +0000

    introduced a changelog and started playing with the blog page

---
+++ public/changelog.md
@@ -0,0 +1,10 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/) 
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+This changelog was set up on 26/10/2016, every bug and issue fixed before that will not be included in the changelog.
+
+## [0.1.0] - 07/03/2017
+- Added changelog
--- public/js/views/blog.jsx
+++ public/js/views/blog.jsx
@@ -1,98 +1,96 @@
 'use strict';
 import React from 'react';
 import Header from "../components/header.jsx";
- 
+
 class Blog extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.blogPost = [
-      {
-        name: "Blog Post 1",
-        date: "Feb 12",
-        textBody: "What is vice? ’Tis what you have often seen. Have this thought ready on all emergences that they are such things as you have often seen: you’ll find all things, earlier or later, just the same."
-      },
-      {
-        name: "Blog Post 2",
-        date: "Feb 13",
-        textBody: "Such matters as fill all histories of the antient, or middle, or present ages: of such things, all cities and families are full. Nothing is new. Every thing is ordinary, and of short duration."
-      },
-      {
-        name: "Blog Post 3",
-        date: "Feb 15",
-        textBody: "How can the grand maxims of life ever become dead in the soul, unless the opinions suitable to them be extinguished? And it is still in your power to revive and kindle again these true opinions."
-      },
-      {
-        name: "Blog Post 4",
-        date: "Feb 16",
-        textBody: "I can always have the sentiments I ought to have about such things; why, then, am I disturbed?"
-      },
-      {
-        name: "Blog Post 5",
-        date: "Feb 17",
-        textBody: "What is external to my soul, is of no consequence to it. Be thus persuaded, and you stand upright and firm."
-      },
-      {
-        name: "Blog Post 6",
-        date: "Feb 19",
-        textBody: "You may revive when you please. Consider things again, as you have done formerly. This is reviving again."
-      }
-    ];
-  }
+	constructor(props) {
+		super(props);
+		this.blogPost = [
+			{
+				id: 1,
+				name: "Blog Post 1",
+				date: "Feb 12",
+				textBody: "What is vice? ’Tis what you have often seen. Have this thought ready on all emergences that they are such things as you have often seen: you’ll find all things, earlier or later, just the same."
+			},
+			{
+				id: 2,
+				name: "Blog Post 2",
+				date: "Feb 13",
+				textBody: "Such matters as fill all histories of the antient, or middle, or present ages: of such things, all cities and families are full. Nothing is new. Every thing is ordinary, and of short duration."
+			},
+			{
+				id: 3,
+				name: "Blog Post 3",
+				date: "Feb 15",
+				textBody: "How can the grand maxims of life ever become dead in the soul, unless the opinions suitable to them be extinguished? And it is still in your power to revive and kindle again these true opinions."
+			},
+			{
+				id: 4,
+				name: "Blog Post 4",
+				date: "Feb 16",
+				textBody: "I can always have the sentiments I ought to have about such things; why, then, am I disturbed?"
+			},
+			{
+				id: 5,
+				name: "Blog Post 5",
+				date: "Feb 17",
+				textBody: "What is external to my soul, is of no consequence to it. Be thus persuaded, and you stand upright and firm."
+			},
+			{
+				id: 6,
+				name: "Blog Post 6",
+				date: "Feb 19",
+				textBody: "You may revive when you please. Consider things again, as you have done formerly. This is reviving again."
+			}
+		];
+	}
 
-  buildBigPosts() {
-    let blogPosts = [];
-    for (let i=0; i<this.blogPost.length; i++) {
-      blogPosts.push(
-        <div className="col-sm-8 col-md-8">
-          <h3>{ this.blogPost[i].name }</h3>
-          <div>{ this.blogPost[i].date }</div>
-          <div>{ this.blogPost[i].textBody }</div>
-        </div>
-      );
-      }
-    return blogPosts;
-  }
+	buildBigPosts() {
+		let blogPosts = [];
+		for (let i = 0; i < this.blogPost.length; i++) {
+			blogPosts.push(
+				<div className="col-sm-8 col-md-8">
+					<h3>{this.blogPost[i].name}</h3>
+					<div>{this.blogPost[i].date}</div>
+					<div>{this.blogPost[i].textBody}</div>
+				</div>
+			);
+		}
+		return blogPosts;
+	}
 
-  buildSmallPosts() {
-    let recentBlogPosts = [];
-    for (let i=0; i<this.blogPost.length; i++) {
-      if(i > this.blogPost.length - 4){
-        recentBlogPosts.unshift(
-        <div className="col-sm-9 col-md-9">
-              <div><a href="/">{ this.blogPost[i].name }</a> - { this.blogPost[i].date } </div>
-        </div>
-      );
-      }
-    }
-    return recentBlogPosts;
-  }
+	buildSmallPosts() {
+		let recentBlogPosts = [];
+		for (let i = 0; i < this.blogPost.length; i++) {
+			if (i > this.blogPost.length - 4) {
+				recentBlogPosts.unshift(
+					<div className="col-sm-12 col-md-12">
+						<div><a href="/">{this.blogPost[i].name}</a> - {this.blogPost[i].date} </div>
+					</div>
+				);
+			}
+		}
+		return recentBlogPosts;
+	}
 
-  render() {
-    return (
-      <div className="blog">
-        <Header/>
-        <div className="container">
-          <div className="col-sm-9">
-            <div className="blog__post card card-2">
-                <h3>Main Body</h3>
-                <div className="container">
-                  {this.buildBigPosts()}
-                </div>
-            </div>
-          </div>
-          <div className="col-sm-3">
-            <div className="blog__recent card card-2">
-              <h3>Recent Posts</h3>
-              <div className="container">
-                { this.buildSmallPosts() }
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="blog">
+				<Header />
+				<div className="container">
+					<div className="col-sm-9">
+						<h3>Main Body</h3>
+						{this.buildBigPosts()}
+					</div>
+					<div className="col-sm-3">
+						<h3>Recent Posts</h3>
+						{this.buildSmallPosts()}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Blog;
